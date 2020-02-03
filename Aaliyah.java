50bc39565229b9d7a3e34ff9de1e527dafa2c77d
Lelouche H <60441025+Leloucheh@users.noreply.github.com>
2020-02-03 12:46:21 -0500

    Add files via upload

---
+++ Aaliyah.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ * @author Aaliyah
+ * Description: A program to declARE VARIABLES
+ * Created    : 02/03/20
+ *
+ **/
+public class Aaliyah {
+	public static void main (String[] args) {
+		//declaration of variable in data type int
+		int age       = 3;
+	    //declaration of variable in data type double
+	    double height = 10.35;
+	    //declaration of variable in data type byte
+	    byte width    = 5;
+	
+	    //assigning string variables
+	    String name1;
+	    String name2;
+	    String name3;
+	 
+	    //declaration of variables in data type string
+	    name1 = "Aaliyah";
+	    name2 = "Dylan";
+	    name3 = "Elena";
+	    
+	    System.out.println(name1);
+	    System.out.println(name1+name3);
+	 
+			
+	
+     }
+}
